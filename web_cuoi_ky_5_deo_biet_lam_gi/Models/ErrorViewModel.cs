252d8a08b186d70ca27d3cafe6f0f91553db3c04
Administrator <Administrator@PMT2012>
2026-06-19 15:37:06 +0700

    Add project files.

---
+++ web_cuoi_ky_5_deo_biet_lam_gi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_cuoi_ky_5_deo_biet_lam_gi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
