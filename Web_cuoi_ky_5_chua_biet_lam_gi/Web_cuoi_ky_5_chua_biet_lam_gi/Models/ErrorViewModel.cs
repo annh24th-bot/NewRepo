07c7cbd279ce51e58a7aeb55b3ff942222cb528a
Administrator <Administrator@PMT2012>
2026-06-24 13:01:05 +0700

    tys

---
+++ Web_cuoi_ky_5_chua_biet_lam_gi/Web_cuoi_ky_5_chua_biet_lam_gi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_cuoi_ky_5_chua_biet_lam_gi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
