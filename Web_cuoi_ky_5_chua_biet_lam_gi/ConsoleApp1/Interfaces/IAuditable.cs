a85f60f5a8bf98c475034cd17eac8f2b6121f959
PMT201 <PMT201@PMT3015>
2026-06-29 13:09:37 +0700

    Complete Lab 01

---
+++ Web_cuoi_ky_5_chua_biet_lam_gi/ConsoleApp1/Interfaces/IAuditable.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Database.Interfaces
+{
+    public interface IAuditable
+    {
+        public Guid ? CreatedBy { get; set;}
+        public DateTime? Createdon { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public DateTime? Modifieddon { get; set; }
+    }
+}
